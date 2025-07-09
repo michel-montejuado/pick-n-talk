9c815ce0bb218110b9ca61a73ad82c532a0d4716
Michel Montejuado <michel.montejuado@gmail.com>
2025-07-09 11:55:22 +0200

    💄

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import Layout from "@/routes/Layout";
-
 import NoMatchPage from "@/routes/NoMatch";
-
 import HomePage from "@/routes/Page";
-
 import BindersPage from "@/routes/binders/Page";
 import BinderPage from "@/routes/binders/[uuid]/Page";
 import BinderEditPage from "@/routes/binders/[uuid]/edit/Page";
-
 import SetupPage from "@/routes/setup/Page";
-
 import UsersPage from "@/routes/users/Page";
 import UserPage from "@/routes/users/[uuid]/Page";
 import UserEditPage from "@/routes/users/[uuid]/edit/Page";
 
+import toggleDarkClass from "@/utils/toggleDarkClass";
+
 export default function App() {
+  useEffect(() => {
+    toggleDarkClass();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -34,7 +36,7 @@ export default function App() {
           <Route path="setup" element={<SetupPage />} />
 
           <Route path="users">
-              <Route index element={<UsersPage />} />
+            <Route index element={<UsersPage />} />
             <Route path=":uuid">
               <Route index element={<UserPage />} />
               <Route path="edit" element={<UserEditPage />} />
