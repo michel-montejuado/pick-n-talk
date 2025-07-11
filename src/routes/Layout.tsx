7f3a779b48319cd248b652d50a9575172fe206cb
Montejuado Michel <michel.montejuado@gmail.com>
2025-07-11 11:24:49 +0200

    ♻️ Refactor layout

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router";
+import { SidebarLayout } from "@/components/ui/sidebar-layout";
+import { Sidebar } from "@/components/ui/sidebar";
+import { Navbar } from "@/components/ui/navbar";
 
 export default function Layout() {
-    return (
-        <>
-            <Outlet />
-        </>
-    )
+  return (
+    <SidebarLayout
+      sidebar={<Sidebar>{/* Add sidebar content here */}</Sidebar>}
+      navbar={<Navbar>{/* Add navbar content here */}</Navbar>}
+    >
+      <Outlet />
+    </SidebarLayout>
+  );
 }
